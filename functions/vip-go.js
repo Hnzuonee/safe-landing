bf49829de53e7700e6f386c3c7308d6933285f36
Hnzuonee <harvillajan@gmail.com>
2025-04-20 18:37:27 +0200

    Update vip-go.js

--- functions/vip-go.js
+++ functions/vip-go.js
@@ -1,7 +1,7 @@
 exports.handler = async () => ({
   statusCode: 302,
   headers: {
-    Location: "https://onlyfans.com/barbarella_xoxo", // sem dej správnou OF URL
+    Location: "https://onlyfans.com/barbarella_xoxo",   // ← pokud máš jinou URL, změň tady
     "Cache-Control": "no-cache"
   },
   body: ""
