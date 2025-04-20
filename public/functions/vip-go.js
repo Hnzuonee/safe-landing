53a00b865ae91a30f55aa0d6a5d49815ee8c761f
Hnzuonee <harvillajan@gmail.com>
2025-04-20 18:29:06 +0200

    Delete public/functions directory

--- public/functions/vip-go.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async () => ({
-  statusCode: 302,
-  headers: {
-    Location: "https://onlyfans.com/barbarella_xoxo", // sem dej správnou OF URL
-    "Cache-Control": "no-cache"
-  },
-  body: ""
-});
